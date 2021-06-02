85591c118b57e84a202bb04da3d75898ef67d8e4
samriddhiK <75959569+samriddhiK@users.noreply.github.com>
2021-06-01 19:11:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+
+function draw() {
+  fill(50, 70, 870);
+  stroke(6000000, 80890854835, 900);
+  rect(-3, 900, 100);
+}
